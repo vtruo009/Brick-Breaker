af185d8449ff9bed5644262cd52d4eefa7cdab2d
Van Truong <vtruo009@wch136-24.cs.ucr.edu>
2019-10-28 12:49:16 -0700

    two LEDs dont light up

--- source/main.c
+++ source/main.c
@@ -1,103 +1,37 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
-
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
-
 /*	Author: vtruo009
  *  Partner(s) Name: An Pho
  *	Lab Section: 023
- *	Assignment: Lab #6  Exercise #1
+ *	Assignment: Lab #8  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-
 #include <avr/io.h>
-#include <avr/interrupt.h>
-
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-volatile unsigned char TimerFlag = 0;
-
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
-
-void TimerOn() {
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	//ADEN: setting this bit enables analog-to-digital conversion
 }
 
-void TimerOff() {
-	TCCR1B = 0x00;
-}
-
-void TimerISR() {
-	TimerFlag = 1;
-}
-
-ISR(TIMER1_COMPA_vect) {
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0) {
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-
-void TimerSet(unsigned long M) {
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-/*void TickSM() {
-	unsigned char tmpB = 0x07;
-	tmpB = ~tmpB;
-	PORTB = tmpB;
-}*/
-
-int main (void)
-{
-	/* Insert application code here, after the board has been initialized. */
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;	
 	DDRB = 0xFF; PORTB = 0x00;
-	TimerSet(1000);
-	TimerOn();
-	unsigned char tmpB = 0x07;
-	while (1) {
-		tmpB = ~tmpB;
-		PORTB = tmpB;
-		//TickSM();
-		while (!TimerFlag);
-		TimerFlag = 0;
-	}
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	ADC_init();
+
+    /* Insert your solution below */
+    while (1) {
+	unsigned short tmp = ADC;
+	unsigned char PB = (char)tmp;
+	unsigned char PD = (char)(tmp >> 4);
+	PORTB = PB;
+	PORTD = PD;
+    }
+    return 1;
 }
