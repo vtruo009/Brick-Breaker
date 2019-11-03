50482cc3e1d514f17d5d6262f8a167496857ebe3
vtruo009 <43657607+vtruo009@users.noreply.github.com>
2019-11-02 21:16:30 -0700

    Completed Part 2

--- Lab9/vtruo009_lab9_part2.c
+++ Lab9/vtruo009_lab9_part2.c
@@ -1,5 +1,5 @@
 /*	Author: Van Truong
- *  	Partner(s) Name: An Pho
+ *  Partner(s) Name: An Pho
  *	Lab Section: 023
  *	Assignment: Lab #9  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
@@ -9,28 +9,90 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+double Notes[8] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+int i = 0;
+volatile unsigned char TimerFlag = 0; //TimerISR() sets this to 1. C programmers should clear to 0
+enum States {SMStart, OFF, WAIT, UP, DOWN, HOLD} state;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+	//AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B; //bit3 = 0: CTC mode (clear timer on compare)
+					//bit2...bit0 = 11: prescaler /64
+					//00001011: 0x0B
+					//So 8MHz clock or 8,000,000 /64 = 125,000 ticks/s
+
+	//AVR putput compare register OCR1A
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	
+	//initiate AVR counter
+	TCNT1 = 0;
+	
+	_avr_timer_cntcurr = _avr_timer_M; // TimerISR will be called every _avr_timer_cntcurr milliseconds
+	
+	//enable global interrupts
+	SREG |= 0x80; //0x90: 10000000
+}
+
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+//set TimerISR() to tick every M ms
+void TimerSet (unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
 void set_PWM(double frequency) {
-	static double current_frequency;
+	static double current_frequency; //keeps track of currently set frequency
+	
+	//will only update the registers when the frequency changes, otherwise allows music play uninterrupted
 	if (frequency != current_frequency) {
-		if (!frequency) { TCCR3B &= 0x08; }
-		else { TCCR3B |= 0x03; }
-		if (frequency < .954) { OCR3A = 0xFFFF; }
-		else if (frequency > 31250) { OCR3A = 0x0000; }
-		else { OCR3A = (short) (8000000 / (128 * frequency)) - 1; }
-		
-		TCNT3 = 0;
-		current_frequency = frequency;
+		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
+		else { TCCR3B |= 0x03; } //resumes/continues timer/counter
+	
+		//prevents OCR3A from overflowing, using prescaler 64
+		//.954 is smallest frequency that will not result in overflow	
+		if  (frequency < 0.954) { OCR3A = 0xFFFF; }
+			
+		//prevents OCR3A from underflowing, using prescaler 64
+		//31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250)  { OCR3A = 0x0000; }
+			
+		//set OCR3A based on desired frequency
+		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
+			
+		TCNT3 = 0; //resets counter
+		current_frequency = frequency; //updates the current frequency
 	}
 }
 
 void PWM_on() {
-	TCCR3A = (1 << COM3A0);
-	TCCR3B = (1 << WGM32) | (1<< CS31) | (1 << CS30);
-	set_PWM(0);
+	TCCR3A = (1 << COM3A0); //COM3A0 toggle PB3 on compare match between counter and OCR3A
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+			//WGM32: when counter TCNT3 matches OCR3A, reset counter
+			//CS31 & CS30: set a prescaler of 64;
 }
 
 void PWM_off() {
@@ -38,120 +100,122 @@ void PWM_off() {
 	TCCR3B = 0x00;
 }
 
-enum States {init, press, on, waiton, button2, button3} states;
-double Notes[8] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25}; 
-unsigned char count = 0x00; //couting the states
-unsigned char flag = 0x00; //flagging on/off
-
-void MusicFnct() {
-	unsigned char A0 = ~PINA & 0x01;	//on/off
-	unsigned char A1 = ~PINA & 0x02;	//up
-	unsigned char A2 = ~PINA & 0x04;	//down
-	switch (states) {
-		case init:
-			states = press;
+void Scale() {
+	unsigned char A0 = ~PINA & 0x01; //ON/OFF
+	unsigned char A1 = ~PINA & 0x02; //UP
+	unsigned char A2 = ~PINA & 0x04; //DOWN
+	
+	switch (state) {
+		case SMStart:
+			state = OFF;
 			break;
-
-		case press:
-			if(A0 && !A1 && !A2) {
-				states = on;
-			}
-			else if(A1 && !A0 && !A2) {
-				states = button2;
-			}
-			else if(A2 && !A1 && !A0) {
-				states = button3;
+			
+		case OFF:
+			if (A0) {
+				state = WAIT;
 			}
 			else {
-				states = init;
+				state = OFF;
 			}
 			break;
-
-		case on:
-			states = waiton;
-			break;
-
-		case waiton:
-			if(A0) {
-				states = init;
+			
+		case WAIT:
+			if (A0) {
+				state = OFF;
+			}
+			else if (A1) {
+				state = UP;
+			}
+			else if (A2) {
+				state = DOWN;
 			}
 			else {
-				states = waiton;
+				state = WAIT;
 			}
 			break;
-
-		case button2:
-			if(A1 && !A0 && !A2) {
-				if(count < 7) {
-					++count;
-				}
-				states = waiton;
+			
+		case UP:
+			if (A0) {
+				state = OFF;
+			}
+			else if (A1) {
+				state = HOLD;
+			}
+			else {
+				state = WAIT;
 			}
 			break;
-
-		case button3:
-			if(A1 && !A0 && !A2) {
-				if(count > 0) {
-					--count;
-				}
-				states = waiton;
+			
+		case DOWN:
+			if (A0) {
+				state = OFF;
+			}
+			else if (A2) {
+				state = HOLD;
+			}
+			else {
+				state = WAIT;
 			}
 			break;
-
-		default:
+			
+		case HOLD:
+			if (A1 || A2) {
+				state = HOLD;
+			}
+			else {
+				state = WAIT;
+			}
 			break;
 	}
 	
-	switch (states) {
-		case init:
+	switch (state) {
+		case SMStart:
 			break;
-
-		case press:
+		
+		case OFF:
+			set_PWM(0);
 			break;
-
-		case waiton:
-			set_PWM(Notes[count]);
+			
+		case WAIT:
+			set_PWM(Notes[i]);
 			break;
-
-		case on:
-			if(flag) {
-				PWM_off();
-				flag = 0;
-			}
-			else {
-				PWM_on();
-				flag = 1;
+		
+		case UP:
+			if (i < 7) {
+				++i;
+				set_PWM(Notes[i]);
 			}
 			break;
-
-		case button2:
-			set_PWM(Notes[count]);
+			
+		case DOWN:
+			if (i > 0) {
+				--i;
+				set_PWM(Notes[i]);
+			}
 			break;
-
-		case button3:
-			set_PWM(Notes[count]);
+			
+		case HOLD:
 			break;
-
+		
 		default:
 			break;
 	}
-	
 }
+
 int main(void)
-{
-	
+{	
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
+	TimerSet(100);
+	TimerOn();
 	PWM_on();
-	//set_PWM(261.63);
-	states = init;
+	
+	state = SMStart;
+	
 	while (1)
 	{
-		MusicFnct();
+		Scale();
+		while (!TimerFlag);
+		TimerFlag = 0;			
 	}
 }
-
-
-
-
-
