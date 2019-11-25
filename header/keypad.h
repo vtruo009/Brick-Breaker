171c798e7fdb5a31f47ef6a525831a54869171e1
Van Truong <vtruo009@wch136-29.cs.ucr.edu>
2019-11-25 12:36:51 -0800

    test

--- header/keypad.h
+++ header/keypad.h
@@ -38,40 +38,40 @@
 //Parameter: None
 //Returns: A keypad button press else '\0'
 unsigned char GetKeypadKey() {
-	static unsigned char let = '\0';
+
 	// Check keys in col 1
 	KEYPADPORT = SetBit(0xFF,COL1,0); // Set Px4 to 0; others 1
 	asm("nop"); // add a delay to allow PORTx to stabilize before checking
-	if ( GetBit(~KEYPADPIN,ROW1) ) { let = '1'; }
-	if ( GetBit(~KEYPADPIN,ROW2) ) { let = '4'; }
-	if ( GetBit(~KEYPADPIN,ROW3) ) { let = '7'; }
-	if ( GetBit(~KEYPADPIN,ROW4) ) { let =  '*'; }
+	if ( GetBit(~KEYPADPIN,ROW1) ) { return '1'; }
+	if ( GetBit(~KEYPADPIN,ROW2) ) { return '4'; }
+	if ( GetBit(~KEYPADPIN,ROW3) ) { return '7'; }
+	if ( GetBit(~KEYPADPIN,ROW4) ) { return '*'; }
 
 	// Check keys in col 2
 	KEYPADPORT = SetBit(0xFF,COL2,0); // Set Px5 to 0; others 1
 	asm("nop"); // add a delay to allow PORTx to stabilize before checking
-	if ( GetBit(~KEYPADPIN,ROW1) ) { let = '2'; }
-	if ( GetBit(~KEYPADPIN,ROW2) ) { let =  '5'; }
-	if ( GetBit(~KEYPADPIN,ROW3) ) { let =  '8'; }
-	if ( GetBit(~KEYPADPIN,ROW4) ) { let =  '0'; }
+	if ( GetBit(~KEYPADPIN,ROW1) ) { return '2'; }
+	if ( GetBit(~KEYPADPIN,ROW2) ) { return '5'; }
+	if ( GetBit(~KEYPADPIN,ROW3) ) { return '8'; }
+	if ( GetBit(~KEYPADPIN,ROW4) ) { return '0'; }
 
 	// Check keys in col 3
 	KEYPADPORT = SetBit(0xFF,COL3,0); // Set Px6 to 0; others 1
 	asm("nop"); // add a delay to allow PORTx to stabilize before checking
-	if ( GetBit(~KEYPADPIN,ROW1) ) {let =  '3'; }
-	if ( GetBit(~KEYPADPIN,ROW2) ) { let =  '6'; }
-	if ( GetBit(~KEYPADPIN,ROW3) ) { let =  '9'; }
-	if ( GetBit(~KEYPADPIN,ROW4) ) { let =  '#'; }
+	if ( GetBit(~KEYPADPIN,ROW1) ) { return '3'; }
+	if ( GetBit(~KEYPADPIN,ROW2) ) { return '6'; }
+	if ( GetBit(~KEYPADPIN,ROW3) ) { return '9'; }
+	if ( GetBit(~KEYPADPIN,ROW4) ) { return '#'; }
 
 	// Check keys in col 4
 	KEYPADPORT = SetBit(0xFF,COL4,0); // Set Px7 to 0; others 1
 	asm("nop"); // add a delay to allow PORTx to stabilize before checking
-	if (GetBit(~KEYPADPIN,ROW1) ) { let =  'A'; }
-	if (GetBit(~KEYPADPIN,ROW2) ) { let =  'B'; }
-	if (GetBit(~KEYPADPIN,ROW3) ) { let = 'C'; }
-	if (GetBit(~KEYPADPIN,ROW4) ) { let =  'D'; }
+	if (GetBit(~KEYPADPIN,ROW1) ) { return 'A'; }
+	if (GetBit(~KEYPADPIN,ROW2) ) { return 'B'; }
+	if (GetBit(~KEYPADPIN,ROW3) ) { return 'C'; }
+	if (GetBit(~KEYPADPIN,ROW4) ) { return 'D'; }
 	
-	return let;
+	return '\0';
 }
 
 #endif //KEYPAD_H
